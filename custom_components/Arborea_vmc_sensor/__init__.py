afe1974b95e36df0ca566aca914f81d5487c3d95
Invernomut0 <lorenzov@MACNBLX035892.local>
2024-06-24 08:43:14 +0200

    Revision 1.2

--- custom_components/Arborea_vmc_sensor/__init__.py
+++ custom_components/Arborea_vmc_sensor/__init__.py
@@ -2,5 +2,16 @@ DOMAIN = "vmc_sensor"
 
 
 async def async_setup(hass, config):
-    """Set up the VMC SENSOR sensor component."""
+    return True
+
+
+async def async_setup_entry(hass, config_entry):
+    hass.async_add_job(
+        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+    )
+    return True
+
+
+async def async_unload_entry(hass, config_entry):
+    await hass.config_entries.async_forward_entry_unload(config_entry, "sensor")
     return True
--- custom_components/Arborea_vmc_sensor/sensor.py
+++ custom_components/Arborea_vmc_sensor/sensor.py
@@ -1,79 +1,136 @@
 import logging
-from homeassistant.components.sensor import SensorEntity
-from homeassistant.const import STATE_OFF, STATE_ON, STATE_UNKNOWN
+import voluptuous as vol
+
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import CONF_NAME, STATE_UNKNOWN
 from homeassistant.core import callback
+from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.event import async_track_state_change_event
+import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = "vmc_sensor"
+CONF_DEVICE_NAME = "device_name"
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_DEVICE_NAME): cv.string,
+    }
+)
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Set up the VMC sensor platform."""
-    sensor = vmc_sensor()
-    async_add_entities([sensor])
-
-    @callback
-    def handle_myhome_event(event):
-        """Handle incoming myhome_message_event."""
-        data = event.data
-        sensor.update_state(data)
-
-    hass.bus.async_listen("myhome_message_event", handle_myhome_event)
-
-    return True
+    return
 
 
-class vmc_sensor(SensorEntity):
-    """Representation of a MyHome Sensor."""
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    device_name = config_entry.data[CONF_DEVICE_NAME]
 
-    def __init__(self):
-        """Initialize the sensor."""
+    vmc_sensor = VmcSensor(device_name)
+    dehumidification_sensor = DehumidificationSensor(device_name)
+    humidity_alarm_sensor = HumidityAlarmSensor(device_name)
+    async_add_entities([vmc_sensor, dehumidification_sensor, humidity_alarm_sensor])
+
+
+class VmcSensor(Entity):
+    def __init__(self, device_name):
+        self._name = f"{device_name} VMC Sensor"
         self._state = STATE_UNKNOWN
-        self._attr_dehumidification = STATE_UNKNOWN
-        self._attr_humidity_alarm = STATE_UNKNOWN
+        self._device_name = device_name
+        self._attributes = {
+            "Deumidificazione": STATE_UNKNOWN,
+            "Allarme_Umidità": STATE_UNKNOWN,
+        }
 
     @property
     def name(self):
-        """Return the name of the sensor."""
-        return "VMC Sensor"
+        return self._name
 
     @property
     def state(self):
-        """Return the state of the sensor."""
         return self._state
 
     @property
     def extra_state_attributes(self):
-        """Return the state attributes."""
-        return {
-            "Deumidificazione": self._attr_dehumidification,
-            "Allarme_Umidità": self._attr_humidity_alarm,
-        }
+        return self._attributes
+
+    async def async_added_to_hass(self):
+        self.hass.bus.async_listen("myhome_message_event", self.handle_event)
 
     @callback
-    def update_state(self, data):
-        """Update the sensor state based on the event data."""
+    def handle_event(self, event):
+        data = event.data
         who = data.get("who")
         where = data.get("where")
         what = data.get("what")
 
         if who == 25 and where == "231":
-            if what == 22:
-                self._state = STATE_ON
-            elif what == 24:
-                self._state = STATE_OFF
+            if what == 24:
+                self._state = "off"
+            elif what == 22:
+                self._state = "on"
+            self.async_write_ha_state()
 
-        if who == 1 and where in ["01", "02"]:
+
+class DehumidificationSensor(Entity):
+    def __init__(self, device_name):
+        self._name = f"{device_name} Deumidificazione"
+        self._state = STATE_UNKNOWN
+        self._device_name = device_name
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def state(self):
+        return self._state
+
+    async def async_added_to_hass(self):
+        self.hass.bus.async_listen("myhome_message_event", self.handle_event)
+
+    @callback
+    def handle_event(self, event):
+        data = event.data
+        who = data.get("who")
+        where = data.get("where")
+        what = data.get("what")
+
+        if who == 1 and where in [1, 2]:
             if what == 1:
-                self._attr_dehumidification = STATE_ON
+                self._state = "on"
             elif what == 0:
-                self._attr_dehumidification = STATE_OFF
+                self._state = "off"
+            self.async_write_ha_state()
 
-        if who == 1 and where == "03":
+
+class HumidityAlarmSensor(Entity):
+    def __init__(self, device_name):
+        self._name = f"{device_name} Allarme Umidità"
+        self._state = STATE_UNKNOWN
+        self._device_name = device_name
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def state(self):
+        return self._state
+
+    async def async_added_to_hass(self):
+        self.hass.bus.async_listen("myhome_message_event", self.handle_event)
+
+    @callback
+    def handle_event(self, event):
+        data = event.data
+        who = data.get("who")
+        where = data.get("where")
+        what = data.get("what")
+
+        if who == 1 and where == 3:
             if what == 0:
-                self._attr_humidity_alarm = STATE_ON
+                self._state = "on"
             elif what == 1:
-                self._attr_humidity_alarm = STATE_OFF
-
-        self.async_write_ha_state()
+                self._state = "off"
+            self.async_write_ha_state()
